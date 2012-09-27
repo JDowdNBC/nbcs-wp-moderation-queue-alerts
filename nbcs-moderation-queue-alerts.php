e9cd0e8bcca9c95f5792d85917bd0dc524f32f89
James Dowd <talraen@gmail.com>
2012-09-27 10:57:09 -0400

    Implemented consolidated options
    Changed the three registered options to a single option, and added a
    validation function to check them

--- nbcs-moderation-queue-alerts.php
+++ nbcs-moderation-queue-alerts.php
@@ -8,12 +8,7 @@ License: GPL2
 */
 
 function nbcs_moderation_queue_alerts_check_queue() {
-//	$options = get_option( 'nbcs-moderation-queue' );
-	$options = array(
-		'minimum' => get_option( 'nbcs-moderation-queue-minimum' ),
-		'email' => get_option( 'nbcs-moderation-queue-email' ),
-		'frequency' => get_option( 'nbcs-moderation-queue-frequency' ),
-	);
+	$options = get_option( 'nbcs-moderation-queue' );
 	$options['email'] = nbcs_moderation_queue_check_email( $options['email'] );
 	
 	if ( false !== get_transient( 'nbcs-moderation-queue-delay' ) || false === $options['minimum'] || false === $options['frequency'] || empty( $options['email'] ) ) {
@@ -48,32 +43,28 @@ function nbcs_moderation_queue_alerts_check_queue() {
 add_action( 'wp_insert_comment', 'nbcs_moderation_queue_alerts_check_queue' );
 
 function nbcs_moderation_queue_alerts_settings_api_init() {
-//	if ( false === get_option( 'nbcs-moderation-queue' ) ) {
-//		$defaults = array(
-//			'minimum' => 100,
-//			'email' => '',
-//			'frequency' => 15,
-//		);
-//		update_option( 'nbcs-moderation-queue', $defaults );
-//	}
+	if ( false === get_option( 'nbcs-moderation-queue' ) ) {
+		$defaults = array(
+			'minimum' => 100,
+			'email' => '',
+			'frequency' => 15,
+		);
+		update_option( 'nbcs-moderation-queue', $defaults );
+	}
 
-	if ( false === get_option( 'nbcs-moderation-queue-minimum' ) ) {
-		update_option( 'nbcs-moderation-queue-minimum', 100 );
-	}
-	if ( false === get_option( 'nbcs-moderation-queue-email' ) ) {
-		update_option( 'nbcs-moderation-queue-email', '' );
-	}
-	if ( false === get_option( 'nbcs-moderation-queue-frequency' ) ) {
-		update_option( 'nbcs-moderation-queue-frequency', 15 );
-	}
-	
 	add_settings_field( 'nbcs-moderation-queue-minimum', 'Moderation Queue Alerts', 'nbcs_moderation_queue_minimum_settings_field', 'discussion', 'default' );
 	
-	register_setting( 'discussion', 'nbcs-moderation-queue-minimum', 'intval' );
-	register_setting( 'discussion', 'nbcs-moderation-queue-email', 'nbcs_moderation_queue_check_email' );
-	register_setting( 'discussion', 'nbcs-moderation-queue-frequency', 'intval' );
+	register_setting( 'discussion', 'nbcs-moderation-queue', 'nbcs_moderation_queue_check_options' );
 }
 add_action( 'admin_init', 'nbcs_moderation_queue_alerts_settings_api_init' );
+
+function nbcs_moderation_queue_check_options( $options ) {
+	$options['minimum'] = intval( $options['minimum'] );
+	$options['email'] = nbcs_moderation_queue_check_email( $options['email'] );
+	$options['frequency'] = intval( $options['frequency'] );
+	
+	return $options;
+}
 
 function nbcs_moderation_queue_check_email( $email ) {
 	$addresses = preg_split( '/[,;]/', $email );
@@ -89,19 +80,14 @@ function nbcs_moderation_queue_check_email( $email ) {
 }
 
 function nbcs_moderation_queue_minimum_settings_field() {
-//	$options = get_option( 'nbcs-moderation-queue' );
-	$options = array(
-		'minimum' => get_option( 'nbcs-moderation-queue-minimum' ),
-		'email' => get_option( 'nbcs-moderation-queue-email' ),
-		'frequency' => get_option( 'nbcs-moderation-queue-frequency' ),
-	);
+	$options = get_option( 'nbcs-moderation-queue' );
 
 ?>
-	<label for="nbcs-moderation-queue-minimum"><?php _e( 'Send an alert email if the comment moderation queue has at least this many comments in it:', 'nbcs-moderation-queue' ); ?> <input type="text" name="nbcs-moderation-queue-minimum" id="nbcs-moderation-queue-minimum" size="2" value="<?php echo esc_attr( $options['minimum'] ); ?>" /></label>
+	<label for="nbcs-moderation-queue-minimum"><?php _e( 'Send an alert email if the comment moderation queue has at least this many comments in it:', 'nbcs-moderation-queue' ); ?> <input type="text" name="nbcs-moderation-queue[minimum]" id="nbcs-moderation-queue-minimum" size="2" value="<?php echo esc_attr( $options['minimum'] ); ?>" /></label>
 	<br />
-	<label for="nbcs-moderation-queue-email"><?php _e( 'Send moderation queue alert emails to the following email address(es):', 'nbcs-moderation-queue' ); ?> <input type="text" name="nbcs-moderation-queue-email" id="nbcs-moderation-queue-email" size="45" value="<?php echo esc_attr( $options['email'] ); ?>" /></label>
+	<label for="nbcs-moderation-queue-email"><?php _e( 'Send moderation queue alert emails to the following email address(es):', 'nbcs-moderation-queue' ); ?> <input type="text" name="nbcs-moderation-queue[email]" id="nbcs-moderation-queue-email" size="45" value="<?php echo esc_attr( $options['email'] ); ?>" /></label>
 	<br />
-	<label for="nbcs-moderation-queue-frequency"><?php _e( 'Do not send another moderation queue alert email until this many minutes have passed:', 'nbcs-moderation-queue' ); ?> <input type="text" name="nbcs-moderation-queue-frequency" id="nbcs-moderation-queue-frequency" size="2" value="<?php echo esc_attr( $options['frequency'] ); ?>" /></label>
+	<label for="nbcs-moderation-queue-frequency"><?php _e( 'Do not send another moderation queue alert email until this many minutes have passed:', 'nbcs-moderation-queue' ); ?> <input type="text" name="nbcs-moderation-queue[frequency]" id="nbcs-moderation-queue-frequency" size="2" value="<?php echo esc_attr( $options['frequency'] ); ?>" /></label>
 <?php
 }
 
